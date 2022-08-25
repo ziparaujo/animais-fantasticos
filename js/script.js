b6a3c3ca15ddb5c3e45cc4b806a7c24f832bb17f
Rodrigo Araujo <ziparaujo@gmail.com>
2022-08-25 19:15:57 -0300

    tooltip refatorada com class

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ import animaScroll from './modules/animacao-scroll.js';
 import Accordion from './modules/accordion.js';
 import TabNav from './modules/tabnav.js';
 import Modal from './modules/modal.js';
-import initTooltip from './modules/tooltip.js';
+import Tooltip from './modules/tooltip.js';
 import initDropdownMenu from './modules/dropdown-menu.js';
 import initMenuMobile from './modules/menu-mobile.js';
 import initdateObject from './modules/data-object.js';
@@ -22,8 +22,10 @@ tabNav.init();
 const modal = new Modal('[data-modal="abrir"]', '[data-modal="fechar"]', '[data-modal="container"]');
 modal.init()
 
+const tooltip = new Tooltip('[data-tooltip]');
+tooltip.init();
+
 animaScroll();
-initTooltip();
 initDropdownMenu();
 initMenuMobile();
 initdateObject();
