18495df94c23cd87610921c1eb7c634a0cea2e41
Rodrigo Araujo <ziparaujo@gmail.com>
2022-08-31 12:01:26 -0300

    acertado caminho da api

--- js/script.js
+++ js/script.js
@@ -38,6 +38,6 @@ menuMobile.init();
 const funcionamento = new Funcionamento('[data-semana]');
 funcionamento.init();
 
-fetchAnimais('../../animaisapi.json', '.numeros-grid');
+fetchAnimais('./animaisapi.json', '.numeros-grid');
 
 fetchBitcoin('https://blockchain.info/ticker', '.btc-preco');
